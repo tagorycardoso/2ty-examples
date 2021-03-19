9a2109300a9edc74b8e04ad23eca4c989d8b0321
Tagory NB <tagory@outlook.com>
2021-03-19 16:07:25 -0300

    Arrays

---
+++ arrays.php
@@ -0,0 +1,83 @@
+<?php
+
+
+
+    //TRÊS FORMAS BÁSICAS DE INSTANCIAR UM ARRAY
+    /*
+
+        1. Atribuindo usando colchetes direto na variável
+        2. Atribuindo usando o método array();
+        3. Usando colchetes em substituição a array()  <=== Metodo preferido
+    */
+
+    $array['Pessoas'][0]['nome'] = 'Tagory Cardoso';
+    $array['Pessoas'][0]['apelido'] = 'tagory';
+
+    $array['Pessoas'][1]['nome'] = 'Rafael Andrade';
+    $array['Pessoas'][1]['apelido'] = 'zangado';
+
+
+    echo 'Primeiro Array: '. "\n";
+    print_r($array);
+    echo "\n\n\n";
+
+    unset($array);
+
+
+    //SEGUNDA FORMA
+
+    $array = array(
+        'Pessoas' => array(
+            array(
+                'nome' => 'Tagory Cardoso',
+                'apelido' => 'tagory'
+            ),
+            array(
+                'nome' => 'Rafael Andrade',
+                'apelido' => 'zangado'
+            ),
+        )
+    );
+
+    echo 'Segundo Array: '. "\n";
+    print_r($array);
+    echo "\n\n\n";
+
+
+    //TERCEIRA FORMA
+
+    $array = [
+        'Pessoas' => [
+            [
+                'nome' => 'Tagory Cardoso',
+                'apelido' => 'tagory'
+            ],
+            [
+                'nome' => 'Rafael Andrade',
+                'apelido' => 'zangado'
+            ]
+        ]
+    ];
+
+
+    //É POSSÍVEL QUE ENCONTRE MUITO NO CÓDIGO JUNÇÃO DE VARIAVEIS DE ARRAYS
+
+    $tagory = [
+        'nome' => 'Tagory Cardoso',
+        'apelido' => 'tagory',
+    ];
+
+    $rafael = [
+        'nome' => 'Rafael Andrade',
+        'apelido' => 'zangado'
+    ];
+
+    $array = [
+        'Pessoas' => [
+            $tagory,
+            $rafael
+        ]
+    ]
+
+
+
