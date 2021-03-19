98d4e56c122948364035798ffe56d2408d34ca13
Tagory NB <tagory@outlook.com>
2021-03-19 16:11:38 -0300

    Laços

---
+++ lacos.php
@@ -0,0 +1,21 @@
+<?php
+
+    require 'arrays.php';
+
+    /*
+        Um dos laços mais utilizados por nós na construção de dados é percorrer arrays utilizando o foreach
+        WHILE e FOR quase nunca são utilizados.
+
+        - Utilizar os dados anteriores para montar o exemplo
+    */
+
+
+    foreach ($array['Pessoas'] as $pessoa) {
+        ?>
+        <tr>
+            <td><?=$pessoa['nome']; ?></td>
+            <td><?=$pessoa['apelido']; ?></td>
+        </tr>
+        <?
+
+    }
