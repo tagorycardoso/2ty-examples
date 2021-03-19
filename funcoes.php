9dd3c5b9029020e52df45a1d3c2d759d96128cc5
Tagory NB <tagory@outlook.com>
2021-03-19 16:38:44 -0300

    Funções

---
+++ funcoes.php
@@ -0,0 +1,23 @@
+<?php
+
+    /*
+        Funções mais utilizadas:
+
+
+        echo
+        print_r
+        array
+        date
+        mktime
+        ksort
+        sort
+        is_array
+        require
+        require_once
+        strlen
+        unset
+        rand
+
+
+    */
+
