5aad8b4fe30ed536ff75d49ace1da8c009cd35b2
Tagory NB <tagory@outlook.com>
2021-03-21 01:45:36 -0300

    Adicionado texto para funções mais utilizadas

--- funcoesdophp.php
+++ funcoesdophp.php
@@ -4,19 +4,19 @@
         Funções mais utilizadas:
 
 
-        echo
-        print_r
-        array
-        date
-        mktime
-        ksort
-        sort
-        is_array
-        require
-        require_once
-        strlen
-        unset
-        rand
+        echo: Para exibir um texto
+        print_r: Para entender a estrutura de um array
+        array: Para criar um array
+        date: Para exibir uma data
+        mktime: Para formar uma data
+        ksort: Para ordenar um array pela chave
+        sort: Para ordenar um array pelo valor
+        is_array: Se for um array
+        require: Requisitar um arquivo
+        require_once: Requisitar um arquivo uma única vez, sem correr o riscco de dar problema por ele já ter sido chamado
+        strlen: Calcula o tamanho de uma string
+        unset: Limpa uma variavel
+        rand: Cria um número aleatório
 
 
     */
